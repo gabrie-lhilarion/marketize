02a8b96acb94880afc25befd7df810cadd9e5a57
Gabriel Hilarion <gabrielhilarion@gmail.com>
2021-12-29 05:40:57 +0100

    Create initial design layout

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,29 @@
 [
     {
         id: "yuy6t7654",
+        name: "Sweet Banana",
+        category: "Fruits",
+        price_type: "multi",
+        price: [
+            { 
+                Onions_of_10kg: "150"
+            },
+            { 
+                Onions_of_20kg: "150"
+            },
+            { 
+                Onions_of_30kg: "150"
+            },
+            { 
+                Onions_of_50kg: "150"
+            },
+        ],
+        largeImage: "link-to-github-user-content",
+        thumbnail: "link-to-thumbnail",
+        availability: "In stock"
+    },
+    {
+        id: "yuy6t7654",
         name: "Sweet Onions",
         category: "Vegetable",
         price_type: "multi",
@@ -19,7 +42,8 @@
             },
         ],
         largeImage: "link-to-github-user-content",
-        thumbnail: "link-to-thumbnail"
+        thumbnail: "link-to-thumbnail",
+        availability: "In stock"
 
-    }
+    },
 ]
